9e633dff387b2b5b09b86b52cd7162d2c55266f7
IanMeyers <meyersi@amazon.com>
2016-04-20 15:02:10 +0100

    Added debug messaging hook and comments to each query for DBA's to determine where/why queries are being run

--- lambda_function.py
+++ lambda_function.py
@@ -23,6 +23,7 @@ database = 'dbname'
 ssl = True
 cluster = 'clustername'
 interval = '1 hour'
+debug = True
 
 ##################
 
@@ -36,9 +37,12 @@ pg8000.paramstyle = "qmark"
 
 def lambda_handler(event, context):
     conn = pg8000.connect(database=database, user=user, password=password, host=host, port=port, ssl=ssl)
+    
+    if debug:
+        print('Succesfully Connected Redshift Cluster')
     cursor = conn.cursor()
 
-    cursor.execute("select \"schema\" || '.' || \"table\" as table, encoded, max_varchar, unsorted, stats_off, tbl_rows, skew_sortkey1, skew_rows from svv_table_info")
+    cursor.execute("select /* Lambda CloudWatch Exporter */ \"schema\" || '.' || \"table\" as table, encoded, max_varchar, unsorted, stats_off, tbl_rows, skew_sortkey1, skew_rows from svv_table_info")
     tables_not_compressed = 0
     max_skew_ratio = 0
     total_skew_ratio = 0
@@ -88,47 +92,47 @@ def lambda_handler(event, context):
     else:
     	avg_skew_sort_ratio = 0
 
-    cursor.execute("SELECT count(a.attname) FROM pg_namespace n, pg_class c, pg_attribute a  WHERE n.oid = c.relnamespace AND c.oid = a.attrelid AND a.attnum > 0 AND NOT a.attisdropped and n.nspname NOT IN ('information_schema','pg_catalog','pg_toast') AND format_encoding(a.attencodingtype::integer) = 'none' AND c.relkind='r' AND a.attsortkeyord != 1")
+    cursor.execute("SELECT /* Lambda CloudWatch Exporter */ count(a.attname) FROM pg_namespace n, pg_class c, pg_attribute a  WHERE n.oid = c.relnamespace AND c.oid = a.attrelid AND a.attnum > 0 AND NOT a.attisdropped and n.nspname NOT IN ('information_schema','pg_catalog','pg_toast') AND format_encoding(a.attencodingtype::integer) = 'none' AND c.relkind='r' AND a.attsortkeyord != 1")
     columns_not_compressed = cursor.fetchone()[0]
     if columns_not_compressed == None:
     	columns_not_compressed = 0
 
-    cursor.execute("SELECT sum(nvl(s.num_qs,0)) FROM svv_table_info t LEFT JOIN (SELECT tbl, COUNT(distinct query) num_qs FROM stl_scan s WHERE s.userid > 1 AND starttime >= GETDATE() - INTERVAL '%s' GROUP BY tbl) s ON s.tbl = t.table_id WHERE t.sortkey1 IS NULL" % interval)
+    cursor.execute("SELECT /* Lambda CloudWatch Exporter */ sum(nvl(s.num_qs,0)) FROM svv_table_info t LEFT JOIN (SELECT tbl, COUNT(distinct query) num_qs FROM stl_scan s WHERE s.userid > 1 AND starttime >= GETDATE() - INTERVAL '%s' GROUP BY tbl) s ON s.tbl = t.table_id WHERE t.sortkey1 IS NULL" % interval)
     queries_scan_no_sort = cursor.fetchone()[0]
     if queries_scan_no_sort == None:
     	queries_scan_no_sort = 0
 
-    cursor.execute("SELECT SUM(w.total_queue_time) / 1000000.0 FROM stl_wlm_query w WHERE w.queue_start_time >= GETDATE() - INTERVAL '%s' AND w.total_queue_time > 0" % interval)
+    cursor.execute("SELECT /* Lambda CloudWatch Exporter */ SUM(w.total_queue_time) / 1000000.0 FROM stl_wlm_query w WHERE w.queue_start_time >= GETDATE() - INTERVAL '%s' AND w.total_queue_time > 0" % interval)
     total_wlm_queue_time = cursor.fetchone()[0]
     if total_wlm_queue_time == None:
     	total_wlm_queue_time = 0
 
-    cursor.execute("SELECT count(distinct query) FROM svl_query_report WHERE is_diskbased='t' AND (LABEL LIKE 'hash%%' OR LABEL LIKE 'sort%%' OR LABEL LIKE 'aggr%%') AND userid > 1 AND start_time >= GETDATE() - INTERVAL '%s'" % interval)
+    cursor.execute("SELECT /* Lambda CloudWatch Exporter */ count(distinct query) FROM svl_query_report WHERE is_diskbased='t' AND (LABEL LIKE 'hash%%' OR LABEL LIKE 'sort%%' OR LABEL LIKE 'aggr%%') AND userid > 1 AND start_time >= GETDATE() - INTERVAL '%s'" % interval)
     total_disk_based_queries = cursor.fetchone()[0]
     if total_disk_based_queries == None:
     	total_disk_based_queries = 0
 
-    cursor.execute("select avg(datediff(ms,startqueue,startwork)) from stl_commit_stats  where startqueue >= GETDATE() - INTERVAL '%s'" % interval)
+    cursor.execute("select /* Lambda CloudWatch Exporter */ avg(datediff(ms,startqueue,startwork)) from stl_commit_stats  where startqueue >= GETDATE() - INTERVAL '%s'" % interval)
     avg_commit_queue = cursor.fetchone()[0]
     if avg_commit_queue == None:
     	avg_commit_queue = 0
 
-    cursor.execute("select count(distinct l.query) from stl_alert_event_log as l where l.userid >1 and l.event_time >= GETDATE() - INTERVAL '%s'" % interval)
+    cursor.execute("select /* Lambda CloudWatch Exporter */ count(distinct l.query) from stl_alert_event_log as l where l.userid >1 and l.event_time >= GETDATE() - INTERVAL '%s'" % interval)
     total_alerts = cursor.fetchone()[0]
     if total_alerts == None:
     	total_alerts = 0
 
-    cursor.execute("select avg(datediff(ms, starttime, endtime)) from stl_query where starttime >= GETDATE() - INTERVAL '%s'" % interval)
+    cursor.execute("select /* Lambda CloudWatch Exporter */ avg(datediff(ms, starttime, endtime)) from stl_query where starttime >= GETDATE() - INTERVAL '%s'" % interval)
     avg_query_time = cursor.fetchone()[0]
     if avg_query_time == None:
     	avg_query_time = 0
 
-    cursor.execute("select sum(packets) from stl_dist where starttime >= GETDATE() - INTERVAL '%s'" % interval)
+    cursor.execute("select /* Lambda CloudWatch Exporter */ sum(packets) from stl_dist where starttime >= GETDATE() - INTERVAL '%s'" % interval)
     total_packets = cursor.fetchone()[0]
     if total_packets == None:
     	total_packets = 0
 
-    cursor.execute("select sum(total) from (select count(query) total from stl_dist where starttime >= GETDATE() - INTERVAL '%s' group by query having sum(packets) > 1000000)" % interval)
+    cursor.execute("select /* Lambda CloudWatch Exporter */ sum(total) from (select count(query) total from stl_dist where starttime >= GETDATE() - INTERVAL '%s' group by query having sum(packets) > 1000000)" % interval)
     queries_traffic = cursor.fetchone()[0]
     if queries_traffic == None:
     	queries_traffic = 0
