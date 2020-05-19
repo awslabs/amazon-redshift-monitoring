684cf640a688c3dd1b7b3e3693755bad52af3fd4
IanMeyers <meyersi@amazon.com>
2020-05-19 08:53:22 +0100

    making deploy type settable as arg

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,12 @@
 
 ver=1.6
 
-for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp dist/redshift-advanced-monitoring-$ver.zip s3://awslabs-code-$r/RedshiftAdvancedMonitoring/redshift-advanced-monitoring-$ver.zip --acl public-read --region $r; done
+if [ "$1" = "" -o "$1" = "bin" ] ; then
+  for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp dist/redshift-advanced-monitoring-$ver.zip s3://awslabs-code-$r/RedshiftAdvancedMonitoring/redshift-advanced-monitoring-$ver.zip --acl public-read --region $r; done
+fi
 
-for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-vpc.yaml s3://awslabs-code-$r/RedshiftAdvancedMonitoring/deploy-vpc.yaml --acl public-read --region $r; done
+if [ "$1" = "" -o "$1" = "yaml" ] ; then
+  for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-vpc.yaml s3://awslabs-code-$r/RedshiftAdvancedMonitoring/deploy-vpc.yaml --acl public-read --region $r; done
 
-for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-non-vpc.yaml s3://awslabs-code-$r/RedshiftAdvancedMonitoring/deploy-non-vpc.yaml --acl public-read --region $r; done
+  for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp deploy-non-vpc.yaml s3://awslabs-code-$r/RedshiftAdvancedMonitoring/deploy-non-vpc.yaml --acl public-read --region $r; done
+fi
